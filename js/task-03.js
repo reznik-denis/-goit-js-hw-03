522349d4eb596c7f124a3a6b2a4deb29010f82e8
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2020-08-04 19:03:30 +0300

    добавляет файлы дз2

---
+++ js/task-03.js
@@ -0,0 +1,23 @@
+// Поиск самого длинного слово в строке с пробелами
+// Напиши функцию findLongestWord(string = ""), которая принимает параметром произвольную строку(в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+
+function findLongestWord(string = "") {
+    // Write code under this line
+    const words = string.split(' ');
+    let longestWord = "";
+    for (const word of words) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+    return longestWord
+}
+
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// 'jumped'
+
+// console.log(findLongestWord('Google do a roll'));
+// 'Google'
+
+console.log(findLongestWord('May the force be with you'));
+// 'force'
---
+++ js/task-05.js
@@ -0,0 +1,21 @@
+// Функция предикат
+// Функция предикат возвращает true или false
+
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.Функция проверяет ее на содержание слов spam и sale.Если нашли запрещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false.Слова в строке могут быть в произвольном регистре.
+
+function checkForSpam(str) {
+    // Write code under this line
+    let message = '';
+    if (str.toLowerCase().includes('spam') || str.toLowerCase().includes('sale')) {
+        message = true;
+    } else { message = false;}
+return message
+}
+
+//console.log(checkForSpam('Latest technology news')); // false
+
+//console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+//console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
